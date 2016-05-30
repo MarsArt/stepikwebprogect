ca7880c45a3c20ac1beebb3844b5378f28193763
Mars <marsart@yandex.ru>
2016-05-30 14:18:11 +0300

    hz

--- web/init.sh
+++ web/init.sh
@@ -2,5 +2,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-gunicorn -c /home/box/web/etc/hello.py hello:application --daemon
-gunicorn -c /home/box/web/etc/django.py wsgi --daemon
+#sudo gunicorn -c /home/box/web/etc/hello.py hello:application --daemon
+#sudo gunicorn -c /home/box/web/etc/django.py ask.wsgi:application --error-logfile /home/box/web/gunic.log --daemon
