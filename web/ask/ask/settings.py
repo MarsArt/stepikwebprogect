a096bebe983caf770ebb51c13f349f26400ecd6e
Mars <marsart@yandex.ru>
2016-06-05 10:59:54 +0300

    work with database

--- web/ask/ask/settings.py
+++ web/ask/ask/settings.py
@@ -32,9 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-  #  'django.contrib.auth',
-     'django.contrib.contenttypes',
-  #  'django.contrib.sessions',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
   #  'django.contrib.messages',
   #  'django.contrib.staticfiles',
     'qa'
@@ -46,8 +46,8 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
-    #'django.contrib.auth.middleware.AuthenticationMiddleware',
-    #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware'
     #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         databace=stepqa
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'NAME': 'stepqa'
     }
 }
 
--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,5 +1,28 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User)
+    likes = models.IntegerField(default=0)
+
+
+
+
+class Answer(models.Model):
+    text=models.TextField()
+    added_at =  models.DateField()
+    question = models.TextField()
+    author = models.ForeignKey(User)
+
+
